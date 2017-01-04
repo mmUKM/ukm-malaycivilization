842e4b22293f0a3fb1454dc135cce031bc463d3b
Jamaludin Rajalu <jrajalu@gmail.com>
2017-01-05 00:31:07 +0800

    No commit message

--- inc/theme-post-type.php
+++ inc/theme-post-type.php
@@ -46,6 +46,10 @@ function atma_template_include( $template ) {
     return $plugindir . '/templates/single-atma_video.php';
   }
 
+  if( $post_type == 'atma_resepi' ) {
+    return $plugindir . '/templates/single-atma_resepi.php';
+  }
+
   if ( is_tax( 'atma_warisan_category' ) ) {
     return $plugindir . '/templates/taxonomy-atma_warisan_category.php';
   }
@@ -89,6 +93,15 @@ function atma_template_include( $template ) {
   if ( is_tax( 'atma_media_category' ) ) {
     return $plugindir . '/templates/taxonomy-atma_media_category.php';
   }
+
+  if ( is_tax( 'atma_resepi_category' ) ) {
+    return $plugindir . '/templates/taxonomy-atma_resepi_category.php';
+  }
+
+  if ( is_tax( 'atma_database' ) ) {
+    return $plugindir . '/templates/taxonomy-atma_database.php';
+  }
+
 
   return $template;
 }
@@ -564,6 +577,58 @@ function atma_custom_post_type() {
     'capability_type'       => 'page',
   );
   register_post_type( 'atma_image', $args );
+
+  /**
+   * Recipe
+   */
+  $labels = array(
+    'name'                  => _x( 'Recipes', 'Post Type General Name', 'atma' ),
+    'singular_name'         => _x( 'Recipe', 'Post Type Singular Name', 'atma' ),
+    'menu_name'             => __( 'Recipe', 'atma' ),
+    'name_admin_bar'        => __( 'Recipe', 'atma' ),
+    'archives'              => __( 'Item Archives', 'atma' ),
+    'parent_item_colon'     => __( 'Parent Item:', 'atma' ),
+    'all_items'             => __( 'All Items', 'atma' ),
+    'add_new_item'          => __( 'Add New Item', 'atma' ),
+    'add_new'               => __( 'Add New', 'atma' ),
+    'new_item'              => __( 'New Item', 'atma' ),
+    'edit_item'             => __( 'Edit Item', 'atma' ),
+    'update_item'           => __( 'Update Item', 'atma' ),
+    'view_item'             => __( 'View Item', 'atma' ),
+    'search_items'          => __( 'Search Item', 'atma' ),
+    'not_found'             => __( 'Not found', 'atma' ),
+    'not_found_in_trash'    => __( 'Not found in Trash', 'atma' ),
+    'featured_Recipe'        => __( 'Featured Recipe', 'atma' ),
+    'set_featured_Recipe'    => __( 'Set featured Recipe', 'atma' ),
+    'remove_featured_Recipe' => __( 'Remove featured Recipe', 'atma' ),
+    'use_featured_Recipe'    => __( 'Use as featured Recipe', 'atma' ),
+    'insert_into_item'      => __( 'Insert into item', 'atma' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this item', 'atma' ),
+    'items_list'            => __( 'Items list', 'atma' ),
+    'items_list_navigation' => __( 'Items list navigation', 'atma' ),
+    'filter_items_list'     => __( 'Filter items list', 'atma' ),
+  );
+  $args = array(
+    'label'                 => __( 'Recipe', 'atma' ),
+    'description'           => __( 'Recipe collections', 'atma' ),
+    'labels'                => $labels,
+    'supports'              => array( 'title', 'revisions', 'page-attributes', ),
+    'hierarchical'          => true,
+    'public'                => true,
+    'show_ui'               => true,
+    'show_in_menu'          => true,
+    //'menu_position'         => 5,
+    'menu_icon'             => get_template_directory_uri() . '/img/database.svg',
+    'show_in_admin_bar'     => false,
+    'show_in_nav_menus'     => false,
+    'can_export'            => true,
+    'has_archive'           => false,
+    'exclude_from_search'   => false,
+    'publicly_queryable'    => true,
+    'query_var'             => 'recipe',
+    'capability_type'       => 'page',
+  );
+  register_post_type( 'atma_recipe', $args );
 }
 add_action( 'init', 'atma_custom_post_type', 0 );
 
@@ -574,8 +639,48 @@ add_action( 'init', 'atma_custom_post_type', 0 );
  * - Tokoh Pemikir & Koleksi Khas
  * - Kamus
  */
-
 function atma_custom_taxonomy() {
+  /**
+   * @name All Databases
+   */
+  $labels = array(
+    'name'                       => _x( 'Databases', 'Taxonomy General Name', 'atma' ),
+    'singular_name'              => _x( 'Database', 'Taxonomy Singular Name', 'atma' ),
+    'menu_name'                  => __( 'Database', 'atma' ),
+    'all_items'                  => __( 'All Items', 'atma' ),
+    'parent_item'                => __( 'Parent Item', 'atma' ),
+    'parent_item_colon'          => __( 'Parent Item:', 'atma' ),
+    'new_item_name'              => __( 'New Item Name', 'atma' ),
+    'add_new_item'               => __( 'Add New Item', 'atma' ),
+    'edit_item'                  => __( 'Edit Item', 'atma' ),
+    'update_item'                => __( 'Update Item', 'atma' ),
+    'view_item'                  => __( 'View Item', 'atma' ),
+    'separate_items_with_commas' => __( 'Separate items with commas', 'atma' ),
+    'add_or_remove_items'        => __( 'Add or remove items', 'atma' ),
+    'choose_from_most_used'      => __( 'Choose from the most used', 'atma' ),
+    'popular_items'              => __( 'Popular Items', 'atma' ),
+    'search_items'               => __( 'Search Items', 'atma' ),
+    'not_found'                  => __( 'Not Found', 'atma' ),
+    'no_terms'                   => __( 'No items', 'atma' ),
+    'items_list'                 => __( 'Items list', 'atma' ),
+    'items_list_navigation'      => __( 'Items list navigation', 'atma' ),
+  );
+  $rewrite = array(
+    'slug'                       => 'atma-database',
+    'with_front'                 => true,
+    'hierarchical'               => false,
+  );
+  $args = array(
+    'labels'                     => $labels,
+    'hierarchical'               => true,
+    'public'                     => true,
+    'show_ui'                    => true,
+    'show_admin_column'          => true,
+    'show_in_nav_menus'          => true,
+    'show_tagcloud'              => false,
+    'rewrite'                    => $rewrite,
+  );
+  register_taxonomy( 'atma_database', array( 'atma_recipe' ), $args );
   /**
    * @name Kategori Warisan
    */
@@ -1033,6 +1138,49 @@ function atma_custom_taxonomy() {
     'rewrite'                    => $rewrite,
   );
   register_taxonomy( 'atma_media_category', array( 'atma_audio', 'atma_image', 'atma_video' ), $args );
+  
+  /**
+   * @name Kategori Resepi
+   */
+  $labels = array(
+    'name'                       => _x( 'Recipe Categories', 'Taxonomy General Name', 'atma' ),
+    'singular_name'              => _x( 'Category', 'Taxonomy Singular Name', 'atma' ),
+    'menu_name'                  => __( 'Category', 'atma' ),
+    'all_items'                  => __( 'All Items', 'atma' ),
+    'parent_item'                => __( 'Parent Item', 'atma' ),
+    'parent_item_colon'          => __( 'Parent Item:', 'atma' ),
+    'new_item_name'              => __( 'New Item Name', 'atma' ),
+    'add_new_item'               => __( 'Add New Item', 'atma' ),
+    'edit_item'                  => __( 'Edit Item', 'atma' ),
+    'update_item'                => __( 'Update Item', 'atma' ),
+    'view_item'                  => __( 'View Item', 'atma' ),
+    'separate_items_with_commas' => __( 'Separate items with commas', 'atma' ),
+    'add_or_remove_items'        => __( 'Add or remove items', 'atma' ),
+    'choose_from_most_used'      => __( 'Choose from the most used', 'atma' ),
+    'popular_items'              => __( 'Popular Items', 'atma' ),
+    'search_items'               => __( 'Search Items', 'atma' ),
+    'not_found'                  => __( 'Not Found', 'atma' ),
+    'no_terms'                   => __( 'No items', 'atma' ),
+    'items_list'                 => __( 'Items list', 'atma' ),
+    'items_list_navigation'      => __( 'Items list navigation', 'atma' ),
+  );
+  $rewrite = array(
+    'slug'                       => 'category-recipe',
+    'with_front'                 => true,
+    'hierarchical'               => false,
+  );
+  $args = array(
+    'labels'                     => $labels,
+    'hierarchical'               => true,
+    'public'                     => true,
+    'show_ui'                    => true,
+    'show_admin_column'          => true,
+    'show_in_nav_menus'          => true,
+    'show_tagcloud'              => false,
+    'rewrite'                    => $rewrite,
+  );
+  register_taxonomy( 'atma_recipe_category', array( 'atma_recipe' ), $args );
+
 }
 add_action( 'init', 'atma_custom_taxonomy', 0 );
 
